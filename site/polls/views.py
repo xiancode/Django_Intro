ec7e2fadb1c9f5d563cd18b853429e25609a6023
Jay@linux <shizhongxian@126.com>
2015-11-26 16:46:13 +0800

    add polls

---
+++ site/polls/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render,get_object_or_404,redirect
+from django.http import HttpResponse,HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.core.context_processors import csrf
+from django.views import generic
+from django.utils import timezone
+from django.contrib.auth import *
+from .models import Question,Choice
+
+# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html' 
+
+def vote(request, question_id):
+    #return HttpResponse("You're voting on question %s." % question_id)
+    p = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        #Redisplay the question voting form.
+        return render(request,'polls/detail.html',{
+                                                   'question':p,
+                                                   'error_message':"You didn't select a choice",
+                                                   })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
+    
+def user_login(request):
+    '''
+    Login
+    '''
+    if request.POST:
+        username = password = ''
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username,password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+            #return redirect('polls/index.html')
+            return redirect('/polls/')
+    ctx = {}
+    ctx.update(csrf(request))
+    return render(request, 'polls/login.html',ctx)
+
+
